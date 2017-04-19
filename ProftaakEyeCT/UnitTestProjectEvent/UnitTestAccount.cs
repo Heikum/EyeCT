cdccc15c7312c3de8a0636837a22c960f652ccc9
Hicham <dhrlaaboudi@gmail.com>
2017-04-20 00:24:40 +0200

    2 unit tests aangemaakt.

---
+++ ProftaakEyeCT/UnitTestProjectEvent/UnitTestAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProftaakEyectEvents;
+using ProftaakEyeCT; 
+
+
+namespace UnitTestProjectEvent
+{
+    [TestClass]
+    public class UnitTestAccount
+    {
+        private Account account;
+        public void TestInitialize()
+        {
+            account = new Account(2, "Student", 24, "richard", "testmail@gmail.com", "1243", false);
+        }
+        [TestMethod]
+        public void TestConstructorAccount()
+        {
+            account = new Account(2, "Student", 24, "richard", "testmail@gmail.com", "1243", false);
+            Assert.AreEqual("richard", account.Username);
+            Assert.AreEqual(2, account.Id);
+            Assert.AreEqual(24, account.Personid);
+            Assert.AreEqual("Student", account.Kind);
+            Assert.AreEqual("testmail@gmail.com", account.Emailadress);
+            Assert.AreEqual(false, account.Rights);
+            Assert.AreEqual("1243", account.Password);
+        }
+    }
+}
---
+++ ProftaakEyeCT/UnitTestProjectEvent/UnitTestPerson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProftaakEyectEvents;
+
+namespace UnitTestProjectEvent
+{
+    [TestClass]
+    public class UnitTestPerson
+    {
+        private Person person;
+        public void TestInitialize()
+        {
+            person = new Person(2, "hicham", "5701ND", "Helmond", "Blinkertsestraat", 5, "0636513443");
+        }
+        [TestMethod]
+        public void TestConstructorPerson()
+        {
+            person = new Person(2, "hicham", "5701ND", "Helmond", "Blinkertsestraat", 5, "0636513443");
+            Assert.AreEqual("hicham", person.Name);
+            Assert.AreEqual(2, person.Id);
+            Assert.AreEqual("5701ND", person.Zipcode);
+            Assert.AreEqual("Helmond", person.City);
+            Assert.AreEqual("Blinkertsestraat", person.Street);
+            Assert.AreEqual(5, person.Number);
+            Assert.AreEqual("0636513443", person.Phonenumber);
+        }
+    }
+}
