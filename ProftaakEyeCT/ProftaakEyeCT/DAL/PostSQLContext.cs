deb23450249f01b44b9c34557eb3e5df8c903c88
Tim S <tim.sanders@student.fontys.nl>
2017-04-20 16:06:29 +0200

    Post Work

--- ProftaakEyeCT/ProftaakEyeCT/DAL/PostSQLContext.cs
+++ ProftaakEyeCT/ProftaakEyeCT/DAL/PostSQLContext.cs
@@ -13,6 +13,88 @@ namespace ProftaakEyeCT.DAL
     {
 
 
+        public List<Post> GetByUsername(string username)
+        {
+            List<Post> posts = new List<Post>();
+
+            using (SqlConnection connection = Database.Connection)
+            {
+                string query = "SELECT P.*, M.*, A.*" +
+                " FROM post p INNER JOIN Media M ON P.MediaID = M.ID" +
+                " INNER JOIN Account A on P.AccountID = A.ID" +
+                " WHERE A.Username = @username";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Post post = this.CreatePostFromReader(reader);
+                            if (post != null)
+                            {
+                                Image image = null;
+                                Video video = null;
+                                Student student = null;
+                                Admin admin = null;
+                                if (reader["MediaiD"] != DBNull.Value)
+                                {
+                                    //Kijken of er een image of een video bij de post hoort
+                                    switch (Convert.ToInt32(reader["MediaType"]))
+                                    {
+                                        case 2:
+                                            image = new Image(
+                                                Convert.ToInt32(reader["Id"]),
+                                                Convert.ToString(reader["ImageName"]),
+                                                Convert.ToString(reader["ImageLink"]));
+                                            post.AddImage(image);
+                                            break;
+
+
+                                        case 1:
+                                            video = new Video(
+                                                Convert.ToInt32(reader["Id"]),
+                                                Convert.ToString(reader["VideoName"]),
+                                                Convert.ToString(reader["VideoLink"]));
+                                            post.AddVideo(video);
+                                            break;
+
+                                    }
+                                }
+                                if (reader["AccountID"] != DBNull.Value)
+                                {
+
+                                    switch (Convert.ToString(reader["Kind"]))
+                                    {
+                                        case "Student":
+                                            student = new Student(
+                                            Convert.ToString(reader["Username"]));
+                                            post.AddStudent(student);
+                                            break;
+
+                                        case "Admin":
+                                            admin = new Admin(
+                                            Convert.ToString(reader["Username"]));
+                                            post.AddAdmin(admin);
+                                            break;
+                                    }
+
+
+                                }
+
+                                posts.Add(post);
+                            }
+
+                        }
+                    }
+
+
+                }
+            }
+            return posts;
+        }
+
         public List<Post> GetAll()
         {
             List<Post> posts = new List<Post>();
@@ -107,18 +189,16 @@ namespace ProftaakEyeCT.DAL
         {
             using (SqlConnection connection = Database.Connection)
             {
-                string query = "INSERT INTO Post (Text, PostDateTime, AccountID)" +
-                    "SELECT Text, PostDateTime, A.ID" +
-                    "FROM Post P  INNER JOIN Media M ON P.MediaID = M.ID" +
-                    "INNER JOIN Account A ON P.AccountID = A.ID";
-
+                string query = "INSERT INTO Post (MediaID, Text, PostDateTime, AccountID)" +
+                    "VALUES (@mediaid, @text, @postdatetime, @accountid)";
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
 
+                    command.Parameters.AddWithValue("@mediaid", post.MediaID);
                     command.Parameters.AddWithValue("@text", post.Text);
                     command.Parameters.AddWithValue("@postdatetime", post.Postdatetime);
+                    command.Parameters.AddWithValue("@accountid", post.AccountID);
 
-                
 
                     try
                     {
@@ -128,8 +208,9 @@ namespace ProftaakEyeCT.DAL
                     {
                         MessageBox.Show(Convert.ToString(e));
                     }
+                    return post;
                 }
-                return post;
+
             }
         }
 
@@ -144,7 +225,8 @@ namespace ProftaakEyeCT.DAL
                 Convert.ToInt32(reader["Id"]),
                 Convert.ToInt32(reader["MediaId"]),
                 Convert.ToString(reader["Text"]),
-                Convert.ToDateTime(reader["PostDateTime"]));
+                Convert.ToDateTime(reader["PostDateTime"]),
+                Convert.ToInt32(reader["AccountID"]));
 
         }
     }
--- ProftaakEyeCT/ProftaakEyeCT/Presentation Layer/PostPlatform.Designer.cs
+++ ProftaakEyeCT/ProftaakEyeCT/Presentation Layer/PostPlatform.Designer.cs
@@ -32,32 +32,45 @@
             this.txtPostPlatformText = new System.Windows.Forms.TextBox();
             this.lblPostPlatformText = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.txtPostMediaLink = new System.Windows.Forms.TextBox();
-            this.lblPostMediaLink = new System.Windows.Forms.Label();
+            this.txtImageLink = new System.Windows.Forms.TextBox();
+            this.lblImageLink = new System.Windows.Forms.Label();
             this.btnAddPost = new System.Windows.Forms.Button();
+            this.txtLoggedInUser = new System.Windows.Forms.TextBox();
+            this.lblLoggedInUser = new System.Windows.Forms.Label();
+            this.rbImageMedia = new System.Windows.Forms.RadioButton();
+            this.rbVideoMedia = new System.Windows.Forms.RadioButton();
+            this.rbGeenMedia = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtVideoName = new System.Windows.Forms.TextBox();
+            this.lblVideoName = new System.Windows.Forms.Label();
+            this.txtVideoLink = new System.Windows.Forms.TextBox();
+            this.lblVideoLink = new System.Windows.Forms.Label();
+            this.txtImageName = new System.Windows.Forms.TextBox();
+            this.lblImageName = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbPostPlatformPosts
             // 
             this.lbPostPlatformPosts.FormattingEnabled = true;
             this.lbPostPlatformPosts.ItemHeight = 16;
-            this.lbPostPlatformPosts.Location = new System.Drawing.Point(631, 43);
+            this.lbPostPlatformPosts.Location = new System.Drawing.Point(338, 21);
             this.lbPostPlatformPosts.Name = "lbPostPlatformPosts";
             this.lbPostPlatformPosts.Size = new System.Drawing.Size(338, 420);
             this.lbPostPlatformPosts.TabIndex = 0;
             // 
             // txtPostPlatformText
             // 
-            this.txtPostPlatformText.Location = new System.Drawing.Point(12, 43);
+            this.txtPostPlatformText.Location = new System.Drawing.Point(11, 190);
             this.txtPostPlatformText.Multiline = true;
             this.txtPostPlatformText.Name = "txtPostPlatformText";
-            this.txtPostPlatformText.Size = new System.Drawing.Size(240, 121);
+            this.txtPostPlatformText.Size = new System.Drawing.Size(250, 121);
             this.txtPostPlatformText.TabIndex = 1;
             // 
             // lblPostPlatformText
             // 
             this.lblPostPlatformText.AutoSize = true;
-            this.lblPostPlatformText.Location = new System.Drawing.Point(9, 23);
+            this.lblPostPlatformText.Location = new System.Drawing.Point(9, 170);
             this.lblPostPlatformText.Name = "lblPostPlatformText";
             this.lblPostPlatformText.Size = new System.Drawing.Size(71, 17);
             this.lblPostPlatformText.TabIndex = 2;
@@ -66,31 +79,31 @@
             // dateTimePicker1
             // 
             this.dateTimePicker1.Enabled = false;
-            this.dateTimePicker1.Location = new System.Drawing.Point(12, 219);
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 366);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(249, 22);
             this.dateTimePicker1.TabIndex = 3;
             this.dateTimePicker1.Value = new System.DateTime(2017, 4, 20, 11, 46, 15, 0);
             // 
-            // txtPostMediaLink
+            // txtImageLink
             // 
-            this.txtPostMediaLink.Location = new System.Drawing.Point(12, 191);
-            this.txtPostMediaLink.Name = "txtPostMediaLink";
-            this.txtPostMediaLink.Size = new System.Drawing.Size(240, 22);
-            this.txtPostMediaLink.TabIndex = 4;
+            this.txtImageLink.Location = new System.Drawing.Point(714, 190);
+            this.txtImageLink.Name = "txtImageLink";
+            this.txtImageLink.Size = new System.Drawing.Size(250, 22);
+            this.txtImageLink.TabIndex = 4;
             // 
-            // lblPostMediaLink
+            // lblImageLink
             // 
-            this.lblPostMediaLink.AutoSize = true;
-            this.lblPostMediaLink.Location = new System.Drawing.Point(9, 171);
-            this.lblPostMediaLink.Name = "lblPostMediaLink";
-            this.lblPostMediaLink.Size = new System.Drawing.Size(80, 17);
-            this.lblPostMediaLink.TabIndex = 5;
-            this.lblPostMediaLink.Text = "Media Link:";
+            this.lblImageLink.AutoSize = true;
+            this.lblImageLink.Location = new System.Drawing.Point(711, 170);
+            this.lblImageLink.Name = "lblImageLink";
+            this.lblImageLink.Size = new System.Drawing.Size(80, 17);
+            this.lblImageLink.TabIndex = 5;
+            this.lblImageLink.Text = "Image Link:";
             // 
             // btnAddPost
             // 
-            this.btnAddPost.Location = new System.Drawing.Point(12, 264);
+            this.btnAddPost.Location = new System.Drawing.Point(12, 411);
             this.btnAddPost.Name = "btnAddPost";
             this.btnAddPost.Size = new System.Drawing.Size(259, 30);
             this.btnAddPost.TabIndex = 6;
@@ -98,20 +111,145 @@
             this.btnAddPost.UseVisualStyleBackColor = true;
             this.btnAddPost.Click += new System.EventHandler(this.btnAddPost_Click);
             // 
+            // txtLoggedInUser
+            // 
+            this.txtLoggedInUser.Enabled = false;
+            this.txtLoggedInUser.Location = new System.Drawing.Point(12, 145);
+            this.txtLoggedInUser.Name = "txtLoggedInUser";
+            this.txtLoggedInUser.Size = new System.Drawing.Size(250, 22);
+            this.txtLoggedInUser.TabIndex = 7;
+            // 
+            // lblLoggedInUser
+            // 
+            this.lblLoggedInUser.AutoSize = true;
+            this.lblLoggedInUser.Location = new System.Drawing.Point(12, 125);
+            this.lblLoggedInUser.Name = "lblLoggedInUser";
+            this.lblLoggedInUser.Size = new System.Drawing.Size(77, 17);
+            this.lblLoggedInUser.TabIndex = 8;
+            this.lblLoggedInUser.Text = "Username:";
+            // 
+            // rbImageMedia
+            // 
+            this.rbImageMedia.AutoSize = true;
+            this.rbImageMedia.Location = new System.Drawing.Point(12, 46);
+            this.rbImageMedia.Name = "rbImageMedia";
+            this.rbImageMedia.Size = new System.Drawing.Size(67, 21);
+            this.rbImageMedia.TabIndex = 9;
+            this.rbImageMedia.TabStop = true;
+            this.rbImageMedia.Text = "Image";
+            this.rbImageMedia.UseVisualStyleBackColor = true;
+            this.rbImageMedia.CheckedChanged += new System.EventHandler(this.rbImageMedia_CheckedChanged);
+            // 
+            // rbVideoMedia
+            // 
+            this.rbVideoMedia.AutoSize = true;
+            this.rbVideoMedia.Location = new System.Drawing.Point(12, 19);
+            this.rbVideoMedia.Name = "rbVideoMedia";
+            this.rbVideoMedia.Size = new System.Drawing.Size(65, 21);
+            this.rbVideoMedia.TabIndex = 10;
+            this.rbVideoMedia.TabStop = true;
+            this.rbVideoMedia.Text = "Video";
+            this.rbVideoMedia.UseVisualStyleBackColor = true;
+            this.rbVideoMedia.CheckedChanged += new System.EventHandler(this.rbVideoMedia_CheckedChanged);
+            // 
+            // rbGeenMedia
+            // 
+            this.rbGeenMedia.AutoSize = true;
+            this.rbGeenMedia.Location = new System.Drawing.Point(12, 73);
+            this.rbGeenMedia.Name = "rbGeenMedia";
+            this.rbGeenMedia.Size = new System.Drawing.Size(106, 21);
+            this.rbGeenMedia.TabIndex = 11;
+            this.rbGeenMedia.TabStop = true;
+            this.rbGeenMedia.Text = "Geen Media";
+            this.rbGeenMedia.UseVisualStyleBackColor = true;
+            this.rbGeenMedia.CheckedChanged += new System.EventHandler(this.rbGeenMedia_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbGeenMedia);
+            this.groupBox1.Controls.Add(this.rbVideoMedia);
+            this.groupBox1.Controls.Add(this.rbImageMedia);
+            this.groupBox1.Location = new System.Drawing.Point(16, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Media Opties:";
+            // 
+            // txtVideoName
+            // 
+            this.txtVideoName.Location = new System.Drawing.Point(714, 41);
+            this.txtVideoName.Name = "txtVideoName";
+            this.txtVideoName.Size = new System.Drawing.Size(250, 22);
+            this.txtVideoName.TabIndex = 13;
+            // 
+            // lblVideoName
+            // 
+            this.lblVideoName.AutoSize = true;
+            this.lblVideoName.Location = new System.Drawing.Point(711, 21);
+            this.lblVideoName.Name = "lblVideoName";
+            this.lblVideoName.Size = new System.Drawing.Size(89, 17);
+            this.lblVideoName.TabIndex = 14;
+            this.lblVideoName.Text = "Video Name:";
+            // 
+            // txtVideoLink
+            // 
+            this.txtVideoLink.Location = new System.Drawing.Point(714, 90);
+            this.txtVideoLink.Name = "txtVideoLink";
+            this.txtVideoLink.Size = new System.Drawing.Size(250, 22);
+            this.txtVideoLink.TabIndex = 15;
+            // 
+            // lblVideoLink
+            // 
+            this.lblVideoLink.AutoSize = true;
+            this.lblVideoLink.Location = new System.Drawing.Point(711, 70);
+            this.lblVideoLink.Name = "lblVideoLink";
+            this.lblVideoLink.Size = new System.Drawing.Size(74, 17);
+            this.lblVideoLink.TabIndex = 16;
+            this.lblVideoLink.Text = "Video Link";
+            // 
+            // txtImageName
+            // 
+            this.txtImageName.Location = new System.Drawing.Point(714, 140);
+            this.txtImageName.Name = "txtImageName";
+            this.txtImageName.Size = new System.Drawing.Size(250, 22);
+            this.txtImageName.TabIndex = 17;
+            // 
+            // lblImageName
+            // 
+            this.lblImageName.AutoSize = true;
+            this.lblImageName.Location = new System.Drawing.Point(711, 120);
+            this.lblImageName.Name = "lblImageName";
+            this.lblImageName.Size = new System.Drawing.Size(91, 17);
+            this.lblImageName.TabIndex = 18;
+            this.lblImageName.Text = "Image Name:";
+            // 
             // PostPlatform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1077, 593);
+            this.ClientSize = new System.Drawing.Size(1087, 593);
+            this.Controls.Add(this.lblImageName);
+            this.Controls.Add(this.txtImageName);
+            this.Controls.Add(this.lblVideoLink);
+            this.Controls.Add(this.txtVideoLink);
+            this.Controls.Add(this.lblVideoName);
+            this.Controls.Add(this.txtVideoName);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblLoggedInUser);
+            this.Controls.Add(this.txtLoggedInUser);
             this.Controls.Add(this.btnAddPost);
-            this.Controls.Add(this.lblPostMediaLink);
-            this.Controls.Add(this.txtPostMediaLink);
+            this.Controls.Add(this.lblImageLink);
+            this.Controls.Add(this.txtImageLink);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.lblPostPlatformText);
             this.Controls.Add(this.txtPostPlatformText);
             this.Controls.Add(this.lbPostPlatformPosts);
             this.Name = "PostPlatform";
             this.Text = "PostPlatform";
+            this.Load += new System.EventHandler(this.PostPlatform_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,8 +261,20 @@
         private System.Windows.Forms.TextBox txtPostPlatformText;
         private System.Windows.Forms.Label lblPostPlatformText;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.TextBox txtPostMediaLink;
-        private System.Windows.Forms.Label lblPostMediaLink;
+        private System.Windows.Forms.TextBox txtImageLink;
+        private System.Windows.Forms.Label lblImageLink;
         private System.Windows.Forms.Button btnAddPost;
+        private System.Windows.Forms.TextBox txtLoggedInUser;
+        private System.Windows.Forms.Label lblLoggedInUser;
+        private System.Windows.Forms.RadioButton rbImageMedia;
+        private System.Windows.Forms.RadioButton rbVideoMedia;
+        private System.Windows.Forms.RadioButton rbGeenMedia;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtVideoName;
+        private System.Windows.Forms.Label lblVideoName;
+        private System.Windows.Forms.TextBox txtVideoLink;
+        private System.Windows.Forms.Label lblVideoLink;
+        private System.Windows.Forms.TextBox txtImageName;
+        private System.Windows.Forms.Label lblImageName;
     }
 }
