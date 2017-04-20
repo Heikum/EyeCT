5a477408108f042c617b2327c2ad0fcd165c5f18
Davey vd Bogaard <daveyvdbogaard@live.nl>
2017-04-20 11:06:13 +0200

    ACcess

--- ProftaakEyeCT/ProftaakEyeCT/Business Layer/Access.cs
+++ ProftaakEyeCT/ProftaakEyeCT/Business Layer/Access.cs
@@ -8,18 +8,23 @@ namespace ProftaakEyectEvents
 {
     public class Access
     {
-        private int rfid { get; set; }
-        private bool status { get; set; }
-        private bool accessstatus { get; set; }
+        public int rfid { get; private set; }
+        public bool accessstatus { get; private set; }
 
-        public void addRFID(Account account, int rfid)
+        public Access(int rfid, bool accessstatus)
+        {
+            this.rfid = rfid;
+            this.accessstatus = accessstatus;
+        }
+        /*public void addRFID(Account account, int rfid)
         {
             //voer in de access tabel een nieuwe rij in met: autoincrement ID, rfid (zelfde als account ID), Account (foreign key?), AccessStatus (automatisch false)
+
         }
         public void removeRFID(Account account)
         {
             //remove uit de accesstabel de rij met het rfid dat het zelfde is als het account ID
-        }
+        }*/
         public bool gainAccess(Account account) //naar binnen
         {
             //haal uit de database voor dat account het RFID ID
