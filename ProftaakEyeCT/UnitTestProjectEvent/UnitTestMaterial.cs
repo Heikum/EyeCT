dcadf61b60e7ea77d4e784bcc589d20fe3651da3
Heikum <dhrlaaboudi@gmail.com>
2017-04-20 13:33:55 +0200

    Unit test done (6).

---
+++ ProftaakEyeCT/UnitTestProjectEvent/UnitTestMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProftaakEyectEvents;
+
+namespace UnitTestProjectEvent
+{
+    [TestClass]
+    public class UnitTestMaterial
+    {
+        private Material material;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.material = new Material(5, "Sok", (decimal)2.2, 24);
+        }
+        [TestMethod]
+        public void TestConstructorMaterial()
+        {
+            Assert.AreEqual(5, this.material.Id); 
+            Assert.AreEqual("Sok", this.material.Name);
+            Assert.AreEqual(24, this.material.Stock);
+            Assert.AreEqual((decimal)2.2, this.material.Price);
+        }
+        [TestMethod]
+        public void TestTostringMaterial()
+        {
+            string result = this.material.ToString();
+            string expected = this.material.Name + " " + this.material.Price + " " + this.material.Stock;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ ProftaakEyeCT/UnitTestProjectEvent/UnitTestVideo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProftaakEyectEvents;
+
+namespace UnitTestProjectEvent
+{
+    [TestClass]
+    public class UnitTestVideo
+    {
+        private Video video;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.video = new Video(3, "Puppy video", "https://www.youtube.com/watch?v=jhpGJb7KPuA");
+        }
+        [TestMethod]
+        public void TestConstructorVideo()
+        {
+            Assert.AreEqual(3, this.video.MediaID);
+            Assert.AreEqual("Puppy video", this.video.VideoName);
+            Assert.AreEqual("https://www.youtube.com/watch?v=jhpGJb7KPuA", this.video.VideoLink);
+        }
+        [TestMethod]
+        public void TestTostringVideo()
+        {
+            string result = this.video.ToString();
+            string expected = this.video.VideoName + "-" + this.video.VideoLink;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
