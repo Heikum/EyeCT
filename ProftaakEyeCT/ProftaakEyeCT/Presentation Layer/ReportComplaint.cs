51b41233e39a6fa6b4438eb19a31bb3d1e020a0e
ItzzYB <youri.bogaars@gmail.com>
2017-05-01 10:13:01 +0200

    rapporteren kut

--- ProftaakEyeCT/ProftaakEyeCT/Presentation Layer/ReportComplaint.cs
+++ ProftaakEyeCT/ProftaakEyeCT/Presentation Layer/ReportComplaint.cs
@@ -1,4 +1,5 @@
-﻿using ProftaakEyectEvents;
+﻿using ProftaakEyeCT.DAL;
+using ProftaakEyectEvents;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,16 +14,31 @@ namespace ProftaakEyeCT.Presentation_Layer
 {
     public partial class ReportComplaint : Form
     {
+        private PostRepository postrepo;
+        private ReactionRepository reactionrepo;
         MediaPlatform mediaplatform = (MediaPlatform)Application.OpenForms["MediaPlatform"];
         public Post post;
         public ReportComplaint()
         {
             InitializeComponent();
+            postrepo = new PostRepository(new PostSQLContext());
+            reactionrepo = new ReactionRepository(new ReactionSQLContext());
         }
 
         private void btnComplaintSend_Click(object sender, EventArgs e)
         {
-            post = mediaplatform.;
+            post = mediaplatform.reportingpost;
+            if(post != null && txtReportComplaint.Text != null)
+            {
+                postrepo.InsertReportedPost(post, txtReportComplaint.Text);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("First fill in your complaint");
+            }
+            
+
         }
     }
 }
